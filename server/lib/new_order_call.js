f20e2dc74f7064fe17119fdf89ff6c5bdba0f3dc
Michael Paszkiewicz <mike@tryhabitat.com>
2015-11-19 15:31:04 -0500

    message handling

--- server/lib/new_order_call.js
+++ server/lib/new_order_call.js
@@ -6,12 +6,23 @@ Router.route('/voice/:name', {
     var self = this;
     var xml = {};
     var xmlResponse = '';
+    var routeOnFinish = baseTwilioURL + 'voice_done/' + this.params.name;
+    var resp = new Twilio.TwimlResponse();
 
+    // resp.say('Welcome to Acme Customer Service!')
+    //        .gather({
+    //            timeout:'10',
+    //            finishOnKey:'*',
+    //            action: routeOnFinish
+    //        }, function() {
+    //           this.say('Hello ' + self.params.name)
+    //               .say('Press 1 for customer service')
+    //               .say('Press 2 for British customer service', { language:'en-gb' })
+    //        });
     //create twiML response document.
     xml.head = "<?xml version='1.0' encoding='UTF-8'?>";
     xml.greet =  "<Response><Say>Yo " + this.params.name + ',' + "there's a new order on Habitat!</Say>";
 
-    var routeOnFinish = baseTwilioURL + 'voice_done/' + this.params.name;
     //notice the extra single quotes, need them or else URL doesn't interpret correctly
     xml.gather = "<Gather action= '" + routeOnFinish + "' finishOnKey='*'>";
 
@@ -23,19 +34,17 @@ Router.route('/voice/:name', {
     for (var key in xml) {
         xmlResponse += xml[key];
     }
-
     //
     this.response.writeHead(200, {'Content-Type': 'text/xml'});
-    this.response.end(xmlResponse);
+    this.response.end(xml);
   }
 });
 
 Router.route('/voice_done/:name', {
   where: 'server',
   action: function() {
-    //store what string of digits customer digits as number
+    //store what string of digits customer enter on keypad
     var digits = Number(this.request.body.Digits);
-    var xml = "<?xml version='1.0' encoding='UTF-8'?><Response><Say>Thank you" + this.params.name + "</Say><Hangup/></Response>";
 
     //parse digit response and do stuff...
     if(digits === 1){
@@ -47,6 +56,7 @@ Router.route('/voice_done/:name', {
       People.update({name: this.params.name}, {$inc: {deniedOrder: 1}}, {multi: true});
     }
 
+    var xml = "<?xml version='1.0' encoding='UTF-8'?><Response><Say>Thank you" + this.params.name + "</Say><Hangup/></Response>";
     //Twilio always needs a response content-type
     this.response.writeHead(200, {'Content-Type': 'text/xml'});
     this.response.end(xml);
--- server/lib/new_order_sms.js
+++ server/lib/new_order_sms.js
@@ -1,3 +1,5 @@
+twilio = Twilio("ACa546052edcae5ae41ba1e2931b4754f3", "39a1d4df7d98776ab7ee44d8239db778");
+
 Router.route('/sms/', {
   where: 'server',
   action: function() {
@@ -6,27 +8,70 @@ Router.route('/sms/', {
     var userPhone = this.request.query.From.substr(2);
     var userObj = People.findOne({phone: userPhone});
 
-    //if it's a vendor, grab company info
-    if(_.contains(userObj.roles, 'vendor')){
-      var bizObj = Businesses.findOne({_id: userObj.bizId});
-      handleVendorResponse(userObj, bizObj, textResponse, function(err, res){
-        if(err){
-          console.log(err);
-        }
-      });
+//NON-USERS
+    if(!userObj){
+      xml = '<Response><Sms>Your campus favorites, delivered. Visit tryhabitat.com to learn more</Sms></Response>';
+      this.response.writeHead(200, {'Content-Type': 'text/xml'});
+      return this.response.end(xml);
     }
 
+//VENDORS
+    else if (_.contains(userObj.roles, 'vendor')){
+      var bizObj = Businesses.findOne({_id: userObj.bizId});
+      console.log('handling vendor response');
+      var acceptedResponses = ["1", "0", "open", "Open", "close", "Close", "helpme", "Helpme"];
 
-    var xml = '<Response><Sms>Thank you for submitting your question!</Sms></Response>';
-    this.response.writeHead(200, {'Content-Type': 'text/xml'});
-    this.response.end(xml);
+      /////
+      //text method response is invalid
+      if(!_.contains(acceptedResponses, textResponse)){
+        xml = '<Response><Sms>Sorry, ' + bizObj.name +  '! This response is invalid. Respond helpme to get deets...</Sms></Response>';
+        this.response.writeHead(200, {'Content-Type': 'text/xml'});
+        return this.response.end(xml);
+      }
+
+      /////
+      //i have no fucking idea whats going on for some reason
+      else if(textResponse == 'helpme') {
+        var resp = new Twilio.TwimlResponse();
+        resp.sms("Text 0 to decline\nText 1 to accept \nText 'open' or 'close' to start and stop receiving orders\nCall support at: 4433869479 ");
+        console.log(resp);
+        xml = "<Response><Sms>Text 0 to decline\nText 1 to accept \nText 'open' or 'close' to start and stop receiving orders\nCall support at: 4433869479 </Sms></Response>";
+        this.response.writeHead(200, {'Content-Type': 'text/xml'});
+        return this.response.end(xml);
+      }
+
+      /////
+      //order declined, set txStatus declined, check for any other orders pending for this biz.
+      //If another order is pending, send text and repeat until all vendor pending with this bizId are accounted for
+      else if (textResponse == '0'){
+          xml = "<Response><Sms>Order declined, if you are too busy, text back 'close' to stop receiving orders</Sms></Response>";
+          this.response.writeHead(200, {'Content-Type': 'text/xml'});
+          return this.response.end(xml);
+      }
+
+      /////
+      //order accepted, set status to 7 and run the 'accept for vendor' click event methods to ping runner
+      else if (textResponse == '1') {
+          xml = "<Response><Sms>'Order accepted, thanks!'</Sms></Response>";
+          this.response.writeHead(200, {'Content-Type': 'text/xml'});
+          return this.response.end(xml);
+      }
+//END VENDOR
+
+//REGISTERED USERS
+    } else {
+      //Check if they have any active orders, send relevant info about tx
+      var usersActiveOrders = transactions.findOne({buyerId: userObj._id, status: {$in: [0, 1, 7]}}, {$orderby: {timeRequested: -1}});
+      if(usersActiveOrders) {
+        xml = "<Response><Sms>Yo " + userObj.profile.fn + ". Here is your current transaction information</Sms></Response>";
+        this.response.writeHead(200, {'Content-Type': 'text/xml'});
+        return this.response.end(xml);
+      } else {
+        xml = "<Response><Sms>Yo " + userObj.profile.fn + ". Got nothin for you...</Sms></Response>";
+        this.response.writeHead(200, {'Content-Type': 'text/xml'});
+        return this.response.end(xml);
+      }
+
+    }
   }
 });
-
-function handleVendorResponse(userObj, bizObj, textResponse){
-  console.log('handling vendor response');
-  var acceptedResponses = ["1", "0", "open", "close", "help"];
-  if(!_.contains(acceptedResponses, textResponse)){
-    return false;
-  }
-}
